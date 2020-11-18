790cddf4ff75193bc128897198cd70b4e3fee827
edoardocollo <edoardo.collo@gmail.com>
2020-11-18 23:27:14 +0100

    bonus

---
+++ b-bonus/assets/js/main.js
@@ -0,0 +1,85 @@
+
+
+
+for (var i = 0; i < 400; i++) {
+  var radius = 100;
+  var angle = Math.random()*Math.PI*2;
+  x = Math.cos(angle)*radius;
+  y = Math.sin(angle)*radius;
+  console.log(x, y);
+
+
+var ranNumX = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+var ranNumY = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+console.log(ranNumX, ranNumY)
+  var contenuto = document.getElementById('box').innerHTML;
+
+  document.getElementById('box').innerHTML = contenuto +  '<div class="cerchio' +
+(Math.floor(Math.random() * (5 - 1 + 1) ) + 1)
+   + ' " style="top:' + (x+ranNumX)  +       '%;left:' + (y+ranNumY) +'%;" ></div>'
+
+}
+// for (var i = 0; i < 100; i++) {
+//   var radius = 100;
+//   var angle = Math.random()*Math.PI*2;
+//   x = Math.cos(angle)*radius;
+//   y = Math.sin(angle)*radius;
+//   console.log(x, y);
+//
+//
+// var ranNumX = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// var ranNumY = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// console.log(ranNumX, ranNumY)
+//   var contenuto = document.getElementById('box').innerHTML;
+//
+//   document.getElementById('box').innerHTML = contenuto +  '<div class="cerchio2" style="top:' + (x+ranNumX)  +       '%;left:' + (y+ranNumY) +'%;" ></div>'
+//
+// }
+// for (var i = 0; i < 100; i++) {
+//   var radius = 100;
+//   var angle = Math.random()*Math.PI*2;
+//   x = Math.cos(angle)*radius;
+//   y = Math.sin(angle)*radius;
+//   console.log(x, y);
+//
+//
+// var ranNumX = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// var ranNumY = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// console.log(ranNumX, ranNumY)
+//   var contenuto = document.getElementById('box').innerHTML;
+//
+//   document.getElementById('box').innerHTML = contenuto +  '<div class="cerchio3" style="top:' + (x+ranNumX)  +       '%;left:' + (y+ranNumY) +'%;" ></div>'
+//
+// }
+// for (var i = 0; i < 100; i++) {
+//   var radius = 100;
+//   var angle = Math.random()*Math.PI*2;
+//   x = Math.cos(angle)*radius;
+//   y = Math.sin(angle)*radius;
+//   console.log(x, y);
+//
+//
+// var ranNumX = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// var ranNumY = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// console.log(ranNumX, ranNumY)
+//   var contenuto = document.getElementById('box').innerHTML;
+//
+//   document.getElementById('box').innerHTML = contenuto +  '<div class="cerchio4" style="top:' + (x+ranNumX)  +       '%;left:' + (y+ranNumY) +'%;" ></div>'
+//
+// }
+// for (var i = 0; i < 100; i++) {
+//   var radius = 100;
+//   var angle = Math.random()*Math.PI*2;
+//   x = Math.cos(angle)*radius;
+//   y = Math.sin(angle)*radius;
+//   console.log(x, y);
+//
+//
+// var ranNumX = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// var ranNumY = Math.ceil(Math.random() * 8) * (Math.round(Math.random()) ? 1 : -1)
+// console.log(ranNumX, ranNumY)
+//   var contenuto = document.getElementById('box').innerHTML;
+//
+//   document.getElementById('box').innerHTML = contenuto +  '<div class="cerchio5" style="top:' + (x+ranNumX)  +       '%;left:' + (y+ranNumY) +'%;" ></div>'
+//
+// }
